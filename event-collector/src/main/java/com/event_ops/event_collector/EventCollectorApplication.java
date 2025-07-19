1ab2e9c3e1f06c2125b73e2985f86f3dc8d963f4
Ricardo Oliveira <ricardoliveira5ro@gmail.com>
2025-07-19 18:27:06 +0100

    project init

---
+++ event-collector/src/main/java/com/event_ops/event_collector/EventCollectorApplication.java
@@ -0,0 +1,13 @@
+package com.event_ops.event_collector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventCollectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventCollectorApplication.class, args);
+	}
+
+}
