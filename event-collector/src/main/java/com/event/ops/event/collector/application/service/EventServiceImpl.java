d831bc42d890147ea0f9accb1d11446ff7b9a970
Ricardo Oliveira <ricardoliveira5ro@gmail.com>
2025-07-27 13:25:38 +0100

    Refactor to Hexagonal Architecture, DDD

---
+++ event-collector/src/main/java/com/event/ops/event/collector/application/service/EventServiceImpl.java
@@ -0,0 +1,41 @@
+package com.event.ops.event.collector.application.service;
+
+import com.event.ops.event.collector.domain.model.Event;
+import com.event.ops.event.collector.infrastructure.kafka.EventProducer;
+import com.event.ops.event.collector.infrastructure.web.dto.EventRequest;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class EventServiceImpl {
+
+    private final EventProducer eventProducer;
+    private final ObjectMapper objectMapper;
+
+    @Autowired
+    public EventServiceImpl(EventProducer eventProducer, ObjectMapper objectMapper) {
+        this.eventProducer = eventProducer;
+        this.objectMapper = objectMapper;
+    }
+
+    public void pushEvent(EventRequest eventRequest) {
+        Event event = new Event();
+
+        event.setEventName(eventRequest.getName());
+        event.setTimestamp(eventRequest.getTimestamp());
+        event.setUserId(eventRequest.getUserId());
+        event.setMetadata(eventRequest.getMetadata());
+
+        try {
+            eventProducer.sendMessage("event-processor", objectMapper.writeValueAsString(event));
+            log.info("Event sent to topic 'event-processor': {}", event);
+
+        } catch (JsonProcessingException e) {
+            log.error(e.getMessage());
+        }
+    }
+}
---
+++ event-collector/src/main/java/com/event/ops/event/collector/infrastructure/web/controller/EventController.java
@@ -0,0 +1,30 @@
+package com.event.ops.event.collector.infrastructure.web.controller;
+
+import com.event.ops.event.collector.application.service.EventServiceImpl;
+import com.event.ops.event.collector.infrastructure.web.dto.EventRequest;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/events")
+public class EventController {
+
+    private final EventServiceImpl eventService;
+
+    @Autowired
+    public EventController(EventServiceImpl eventService) {
+        this.eventService = eventService;
+    }
+
+    @PostMapping
+    public ResponseEntity<String> processEvent(@Valid @RequestBody EventRequest eventRequest) {
+        eventService.pushEvent(eventRequest);
+
+        return ResponseEntity.ok("Event received");
+    }
+}
