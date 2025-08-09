1252279c5917183f407e77e21e57b0a3c592bee1
Ricardo Oliveira <ricardoliveira5ro@gmail.com>
2025-08-09 21:59:28 +0100

    ClientEntity and table flyway migration

---
+++ database/src/main/java/com/event/ops/database/entity/ClientEntity.java
@@ -0,0 +1,42 @@
+package com.event.ops.database.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "T_CLIENT")
+public class ClientEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id")
+    private UUID id;
+
+    @Column(name = "client_id", unique = true, nullable = false)
+    private String clientId;
+
+    @Column(name = "client_secret", nullable = false)
+    private String clientSecret;
+
+    @Column(name = "client_name", nullable = false)
+    private String clientName;
+
+    @Column(name = "active", nullable = false)
+    private boolean active = true;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private Instant createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private Instant updatedAt;
+}
